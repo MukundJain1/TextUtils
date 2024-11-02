c8cea14a38894cbe3b4b5473b8cb1c3cd05990a6
Mukund <mukundjain2803@gmail.com>
2024-11-02 23:11:38 +0530

    Updates

---
+++ precache-manifest.565405d69f554c858d7db789886878f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878fa32fb58123010495ad1deac3d013",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "c489c2713e130e92f0ff",
+    "url": "/TextUtils/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "8400bafdb7b7ff8beb2d",
+    "url": "/TextUtils/static/js/2.6c0dec52.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/TextUtils/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "c489c2713e130e92f0ff",
+    "url": "/TextUtils/static/js/main.608b0146.chunk.js"
+  },
+  {
+    "revision": "292012d43ca6d83acd9e",
+    "url": "/TextUtils/static/js/runtime~main.35a28519.js"
+  }
+]);
